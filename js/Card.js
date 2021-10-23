a9bbd9c30e46b13ab1dd3b48c380d75ac7bc1110
Dmitry <mitinov.dmitriy@gmail.com>
2021-10-23 11:53:17 +0300

    added skeleton for editing card

--- js/Card.js
+++ js/Card.js
@@ -4,6 +4,8 @@ export default class Card {
     constructor({name, email, title, body, id}) {
         this.data = {name, email, title, body, postId: id};
         this.element = document.createElement("div");
+        this.cardTitle = document.createElement("h5");
+        this.cardText = document.createElement("p");
     }
     render(outputElem){
         const output = document.querySelector(outputElem);
@@ -14,25 +16,31 @@ export default class Card {
         this.element.prepend(header);
         const body = document.createElement("div");
         body.classList.add("card-body");
-        const cardTitle = document.createElement("h5");
-        cardTitle.classList.add("card-title");
-        cardTitle.innerText = `${this.data.title}`;
-        const cardText = document.createElement("p");
-        cardText.classList.add("card-text");
-        cardText.innerText = `${this.data.body}`
+        this.cardTitle.classList.add("card-title");
+        this.cardTitle.innerText = `${this.data.title}`;
+        const editTitle = document.createElement("span");
+        editTitle.classList.add("titleEdit");
+        this.cardText.classList.add("card-text");
+        this.cardText.innerText = `${this.data.body}`
+        const editText = document.createElement("span");
+        editText.classList.add("textEdit");
         const removeBtn = document.createElement("button");
         removeBtn.className = "btn btn-danger post_remove_btn";
         removeBtn.innerText = "Удалить пост";
         removeBtn.addEventListener("click", this.removeCard)
-        body.append(cardTitle);
-        body.append(cardText);
+        body.append(this.cardTitle);
+        body.append(editTitle);
+        body.append(this.cardText);
+        body.append(editText);
         body.append(removeBtn);
         this.element.append(body);
         output.append(this.element);
     }
     removeCard = () => {
-        console.log(this)
         Request.deleteItem(`https://ajax.test-danit.com/api/json/posts/${this.data.postId}`).then(() => this.element.remove());
+
+    }
+    editCard = () => {
 
     }
 }
